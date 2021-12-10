4ccac35b9d3347072cd6f9672afc354bcadd0937
EricGPTI <ericgpti@gmail.com>
2021-12-09 21:21:27 -0300

    Setup app django

---
+++ api/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.db.models.deletion import CASCADE
+from django.db.models.fields import AutoField
+
+
+# class Department(models.Model):
+#     id = models.IntegerField(primary_key=True, AutoField=True, unique=True)
+#     department = models.CharField(max_length=30)
+# 
+# class Employees(models.Model):
+#     id = models.IntegerField(primary_key=True, unique=True, AutoField=True)
+#     name = models.CharField(max_length=100)
+#     email = models.EmailField()
+#     department_id = models.ForeignKey(Department, on_delete=CASCADE)
+
---
+++ api/tests/test_endpoints.py
@@ -0,0 +1,12 @@
+from requests import get
+
+url = "http://127.0.0.1:8000/"
+
+def test_endpoint_employees_status_code_200():
+    resp = get(url + "employees/")
+    assert resp.status_code == 200
+
+
+def test_employee_id_1():
+    resp = get(url + "employees/")
+    assert resp.status_code == 200

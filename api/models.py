9602e0b31348808564090cb6f98fc3440f06d324
EricGPTI <ericgpti@gmail.com>
2021-12-10 22:04:35 -0300

    ajuste flake8 e correções pep8, ajuste urls e models

--- api/models.py
+++ api/models.py
@@ -3,13 +3,13 @@ from django.db.models.deletion import CASCADE
 from django.db.models.fields import AutoField
 
 
-# class Department(models.Model):
-#     id = models.IntegerField(primary_key=True, AutoField=True, unique=True)
-#     department = models.CharField(max_length=30)
-# 
-# class Employees(models.Model):
-#     id = models.IntegerField(primary_key=True, unique=True, AutoField=True)
-#     name = models.CharField(max_length=100)
-#     email = models.EmailField()
-#     department_id = models.ForeignKey(Department, on_delete=CASCADE)
+class Department(models.Model):
+    id = models.IntegerField(primary_key=True, AutoField=True, unique=True)
+    department = models.CharField(max_length=30)
 
+
+class Employees(models.Model):
+    id = models.IntegerField(primary_key=True, unique=True, AutoField=True)
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    department_id = models.ForeignKey(Department, on_delete=CASCADE)
--- api/views.py
+++ api/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 
+
 def employees(requests):
     return HttpResponse("Estou on")

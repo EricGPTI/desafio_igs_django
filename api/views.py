0704a2baffbbd4afb4c057acfe97701d32869428
EricGPTI <ericgpti@gmail.com>
2021-12-12 17:58:34 -0300

    Criação de view para employees

--- api/views.py
+++ api/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render, HttpResponse
+from api.models import Employee, Department
+from api.serializer import EmployeeSerializer, DepartmentSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
 
+@api_view(['GET'])
+def employees(request):
+    if request.method == 'GET':
+        employees = Employee.objects.all()
+        serializer = EmployeeSerializer(employees, many=True)
+        return Response(serializer.data)
 
-def employees(requests):
-    return HttpResponse("Estou on")
+#@api_view(['POST'])
+#def employees(requests):
+#    if request.method == 'POST':
+#        data = request.data
+#        department = Department.objects.get(id=data['department_id'])
+#        if department is None:
+#            return Response(status=status.HTTP_400_BAD_REQUEST)
+#        employee = Employee(name=data['name'].Capitalize(), email=data['email'].lower(), data['department'])
+#        user = (data['name'], data['email'], data['department'])
+#
